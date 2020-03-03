0662eeaa678b225be5da896f093c1196f2eb6670
mapoulo <nkwejustice@gmail.com>
2020-03-03 11:39:30 +0200

    Nkwe

--- src/app/messages/messages.page.ts
+++ src/app/messages/messages.page.ts
@@ -70,7 +70,7 @@ export class MessagesPage implements OnInit  {
     
     setTimeout(() => {
       this.scrollToBottomOnInit(); 
-    }, 100);
+    }, 20);
     
 
     this.db.collection("Message").get().then(item => {
@@ -107,7 +107,11 @@ export class MessagesPage implements OnInit  {
         this.ShowMessage = true
         this.db.collection("Message").orderBy("time", "asc").onSnapshot(data => {
 
-          this.MyMessages  = []
+          this.MyMessages  = [];
+
+          setTimeout(() => {
+            this.scrollToBottomOnInit(); 
+          }, 20);
 
           data.forEach(item => {
             if(item.data().uid == firebase.auth().currentUser.uid){
@@ -116,6 +120,10 @@ export class MessagesPage implements OnInit  {
               
             }
           })
+          
+          setTimeout(() => {
+            this.scrollToBottomOnInit(); 
+          }, 20);
         })
 
 
@@ -155,7 +163,7 @@ export class MessagesPage implements OnInit  {
           status : "NotRead",
           cmsUid : "CMS",
           number : this.MyNumber,
-          time : moment().format('MMMM Do YYYY, h:mm:ss a'),
+          time : moment().format('MMMM Do YYYY, h:mm a'),
           uid : firebase.auth().currentUser.uid,
          
         })
@@ -169,7 +177,7 @@ export class MessagesPage implements OnInit  {
 
     setTimeout(() => {
       this.scrollToBottomOnInit(); 
-    }, 100);
+    }, 20);
      
 
    }
